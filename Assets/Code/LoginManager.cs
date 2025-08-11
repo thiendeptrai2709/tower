7237360cb946814c95ca28f3a02a178214b22b6a
Tên bạn <email@example.com>
2025-08-11 20:24:26 +0700

    LoginUi (Chưa code)

---
+++ Assets/Code/LoginManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class LoginManager : MonoBehaviour
+{
+    public GameObject panelLogin;
+    public GameObject panelRegister;
+
+    void Start()
+    {
+        panelLogin.SetActive(false);
+        panelRegister.SetActive(false);
+    }
+
+    public void OnClickLogin()
+    {
+        panelLogin.SetActive(true);
+        panelRegister.SetActive(false);
+    }
+
+    public void OnClickRegister()
+    {
+        panelRegister.SetActive(true);
+        panelLogin.SetActive(false);
+    }
+
+    public void OnClickBack()
+    {
+        panelLogin.SetActive(false);
+        panelRegister.SetActive(false);
+    }
+}
